ce91bdb63f7efc4a77294175ad1f52abea689daf
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-04 12:09:33 +0000

    Remove g3log (#10)

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,96 +1,64 @@
 /*
-    Copyright (c) 2019 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Copyright (c) 2019-2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/core/blob/main/LICENSE.txt
 */
 
 #include "Log.h"
 
+using namespace Ishiko;
+
 namespace Nemu
 {
 
-Log::Log(const std::string& filenamePrefix)
+Log::Log(LoggingSink& sink)
+    : m_logger(sink)
 {
-    m_worker = g3::LogWorker::createLogWorker();
-    m_handle = m_worker->addDefaultLogger(filenamePrefix, ".");
-    g3::initializeLogging(m_worker.get());
-    m_handle->call(&g3::FileSink::overrideLogDetails, &FormatMessage);
-}
-
-Log::Log(const std::string& filenamePrefix, ELogDestination destination)
-{
-    // TODO
-
-    m_worker = g3::LogWorker::createLogWorker();
-    m_handle = m_worker->addDefaultLogger(filenamePrefix, ".");
-    g3::initializeLogging(m_worker.get());
-    m_handle->call(&g3::FileSink::overrideLogDetails, &FormatMessage);
+    // TODO: need to support the following logging format
+    // out.append(message.timestamp({"%Y-%m-%dT%H:%M:%S.%f6"}) + " (" + message.threadID() + ") [" + message.file() + ":"
+     //   + message.line() + "] " + message.level() + " ");
 }
 
 void Log::onApplicationStarting(const Application& source)
 {
-    LOG(INFO) << "Application starting";
+    m_logger.info("Application starting");
 }
 
 void Log::onApplicationStarted(const Application& source)
 {
     size_t running = source.servers().numberOfRunningServers();
-    LOG(INFO) << "Application started (" << running << " server" << ((running != 1) ? "s" : "") << " running)";
+    m_logger.info("Application started ({} server{} running", running, ((running != 1) ? "s" : ""));
 }
 
 void Log::onApplicationStopping(const Application& source)
 {
-    LOG(INFO) << "Application stopping";
+    m_logger.info("Application stopping");
 }
 
 void Log::onApplicationStopped(const Application& source)
 {
     size_t running = source.servers().numberOfRunningServers();
-    LOG(INFO) << "Application stopped (" << running << " server" << ((running != 1) ? "s" : "") << " running)";
+    m_logger.info("Application stopped ({} server{} running", running, ((running != 1) ? "s" : ""));
 }
 
 void Log::onServerStarted(const Server& source)
 {
-    LOG(INFO) << "Server started";
+    m_logger.info("Server started");
 }
 
 void Log::onServerStopped(const Server& source)
 {
-    LOG(INFO) << "Server stopped";
+    m_logger.info("Server stopped");
 }
 
 void Log::onConnectionOpened(const Server& source, const std::string& sourceAddress)
 {
-    LOG(INFO) << "Connection from " << sourceAddress << " opened";
+    m_logger.info("Connection from {} opened", sourceAddress);
 }
 
 void Log::onConnectionClosed(const Server& source, const std::string& sourceAddress)
 {
-    LOG(INFO) << "Connection from " << sourceAddress << " closed";
-}
-
-std::string Log::FormatMessage(const g3::LogMessage& message)
-{
-    std::string out;
-    out.append(message.timestamp({"%Y-%m-%dT%H:%M:%S.%f6"}) + " (" + message.threadID() + ") [" + message.file() + ":"
-        + message.line() + "] " + message.level() + " ");
-    return out;
+    m_logger.info("Connection from {} closed", sourceAddress);
 }
 
 }
