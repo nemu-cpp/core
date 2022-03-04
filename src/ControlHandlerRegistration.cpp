f6721eefd7cd7b1078021bbf365270e44cd9ae03
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-04 12:09:02 +0000

    Updates

--- src/ControlHandlerRegistration.cpp
+++ src/ControlHandlerRegistration.cpp
@@ -1,30 +1,13 @@
 /*
-    Copyright (c) 2019 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Copyright (c) 2019-2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/core/blob/main/LICENSE.txt
 */
 
 #include "ControlHandlerRegistration.h"
 
 #ifdef _WIN32
 #include "Applications.h"
-#include <g3log/g3log.hpp>
 #include <windows.h>
 
 namespace Nemu
@@ -38,7 +21,8 @@ BOOL WINAPI ControlHandler(DWORD fdwCtrlType)
     switch (fdwCtrlType)
     {
     case CTRL_C_EVENT:
-        LOG(INFO) << "Ctrl-C received, stopping all applications";
+        // TODO
+        // LOG(INFO) << "Ctrl-C received, stopping all applications";
         Applications::StopAllApplications();
         return TRUE;
 
