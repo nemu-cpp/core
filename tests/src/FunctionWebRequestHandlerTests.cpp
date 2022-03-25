9083557e8d7c8e965718715fd48842d463ba872b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-25 21:35:20 +0000

    Add test

--- tests/src/FunctionWebRequestHandlerTests.cpp
+++ tests/src/FunctionWebRequestHandlerTests.cpp
@@ -5,15 +5,20 @@
 */
 
 #include "FunctionWebRequestHandlerTests.hpp"
+#include "Helpers/TestWebRequest.hpp"
+#include "Helpers/TestWebResponseBuilder.hpp"
 #include "Nemu/Core/Web/RequestHandlers/FunctionWebRequestHandler.hpp"
+#include <iostream>
 
 using namespace Ishiko;
 using namespace Nemu;
+using namespace std;
 
 FunctionWebRequestHandlerTests::FunctionWebRequestHandlerTests(const TestNumber& number, const TestContext& context)
     : TestSequence(number, "FunctionWebRequestHandler tests", context)
 {
     append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+    append<HeapAllocationErrorsTest>("run test 1", RunTest1);
 }
 
 void FunctionWebRequestHandlerTests::ConstructorTest1(Test& test)
@@ -24,5 +29,32 @@ void FunctionWebRequestHandlerTests::ConstructorTest1(Test& test)
         {
         });
 
+    ISHIKO_TEST_PASS();
+}
+
+void FunctionWebRequestHandlerTests::RunTest1(Test& test)
+{
+    int data = 0;
+    FunctionWebRequestHandler requestHandler(
+        [&data](const WebRequest& request, WebResponseBuilder& response, void* handlerData, Logger& logger)
+        {
+            response.setStatus(200);
+            response.body() = "body";
+            data = 1;
+        });
+
+    TestWebRequest request("/");
+    Views views;
+    TestWebResponseBuilder responseBuilder(views);
+
+    // TODO: null log
+    StreamLoggingSink sink(cout);
+    Logger log(sink);
+
+    requestHandler.run(request, responseBuilder, log);
+
+    ISHIKO_TEST_FAIL_IF_NEQ(responseBuilder.statusCode(), 200);
+    ISHIKO_TEST_FAIL_IF_NEQ(responseBuilder.body(), "body");
+    ISHIKO_TEST_FAIL_IF_NEQ(data, 1);
     ISHIKO_TEST_PASS();
 }
