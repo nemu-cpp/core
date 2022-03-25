184138b38edd6975c113add5f2a1d9d144e9fe89
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-25 13:10:54 +0000

    Add files

---
+++ include/Nemu/Core/WebRequestHandler.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/core/blob/main/LICENSE.txt
+*/
+
+#ifndef _NEMU_CPP_CORE_WEBREQUESTHANDLER_HPP_
+#define _NEMU_CPP_CORE_WEBREQUESTHANDLER_HPP_
+
+#endif
