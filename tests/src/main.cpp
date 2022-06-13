f990ec56cb71609766494ee2022a30cd58c975c0
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-13 13:28:37 +0100

    Update test code after changes to dependencies

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -14,9 +14,9 @@ int main(int argc, char* argv[])
 {
     TestHarness theTestHarness("NemuCore");
 
-    theTestHarness.context().setTestDataDirectory("../../data");
-    theTestHarness.context().setTestOutputDirectory("../../output");
-    theTestHarness.context().setReferenceDataDirectory("../../reference");
+    theTestHarness.context().setDataDirectory("../../data");
+    theTestHarness.context().setOutputDirectory("../../output");
+    theTestHarness.context().setReferenceDirectory("../../reference");
 
     TestSequence& theTests = theTestHarness.tests();
     theTests.append<ConfigurationTests>();
